958d55fd18d838f6568af455d6ab708edcd42d93
lisachandra <lisachandra's email is private>
2021-12-24 14:13:05 +0700

    Built and published by Moonwave

---
+++ assets/js/52476470.55b5ffda.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[229],{82824:function(e){e.exports=JSON.parse('[{"type":"link","href":"/api/WrapperService","label":"\u200bWrapper\u200bService"},{"type":"link","href":"/api/WrappedInstance","label":"\u200bWrapped\u200bInstance"}]')}}]);
