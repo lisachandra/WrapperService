ef0dcf4b5aad2485016adf7d8257e4a1f0e485d2
lisachandra <lisachandra's email is private>
2022-01-15 14:49:17 +0700

    fix array

--- test/build.bash
+++ test/build.bash
@@ -15,7 +15,7 @@ fi
 declare -A DEPENDENCIES
 
 while read line; do
-    KEY=$(awk -v LINE="$line" -F '[@/"]' '{if (NR == LINE); print $2 "_" $3}' DEPENDENCIES.toml)
+    KEY=$(awk -v LINE="$line" -F'[@/"]' '{if (NR == LINE); print $2 "_" $3}' DEPENDENCIES.toml)
     VALUE=$(awk -v LINE="$line" '{if (NR == LINE); print $1}' DEPENDENCIES.toml)
 
     DEPENDENCIES+=(["$KEY"]="$VALUE")
@@ -28,7 +28,7 @@ mkdir Packages/_Index/zxibs_wrapperservice
 cd Packages/_Index/zxibs_wrapperservice
 
 for key in "${!DEPENDENCIES[@]}"; do
-    DEPENDENCY_NAME=$(echo "$key" | awk -v -F '_' '{print $2}')
+    DEPENDENCY_NAME=$(echo "$key" | awk -v -F'_' '{print $2}')
     DEPENDENCY_PATH_NAME=$(echo "$DEPENDENCIES_PATH_NAME" | awk -v PATTERN="$key" '$0~PATTERN')
 
     echo "${key} ${DEPENDENCY_NAME} ${DEPENDENCY_PATH_NAME} ${DEPENDENCIES[${key}]}"
