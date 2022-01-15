b2f4fc99546180fc25079bf5c6309cd7acb522a9
lisachandra <lisachandra's email is private>
2022-01-15 07:52:51 +0700

    added printing output for debugging

--- test/build.bash
+++ test/build.bash
@@ -23,6 +23,8 @@ while IFS= read -r line; do
     KEY=$(awk -v LINE="$line" -F'[@/"]' '{if (NR == LINE); print $2 "_" $3}' DEPENDENCIES.toml)
     VALUE=$(awk -v LINE="$line" '{if (NR == LINE); print $1}' DEPENDENCIES.toml)
 
+    echo "${KEY} ${VALUE}"
+
     DEPENDENCIES+=(["$KEY"]="$VALUE")
 done < DEPENDENCIES.toml
 
@@ -34,8 +36,7 @@ for key in ${!DEPENDENCIES[@]}; do
     DEPENDENCY_NAME=$(echo "$key" | awk -v FS=_ '{print $2}')
     DEPENDENCY_PATH_NAME=$(echo "$DEPENDENCIES_LIST" | awk -v PATTERN="$key" '$0~PATTERN')
 
-    echo "${key} ${DEPENDENCY_PATH_NAME} ${DEPENDENCY_NAME} ${DEPENDENCIES[${key}]}"
-
+    echo "return require(script.Parent.Parent['${DEPENDENCY_PATH_NAME}']['${DEPENDENCY_NAME}'])"
     echo "return require(script.Parent.Parent['${DEPENDENCY_PATH_NAME}']['${DEPENDENCY_NAME}'])" > "${DEPENDENCIES[${key}]}.lua"
 done
 
