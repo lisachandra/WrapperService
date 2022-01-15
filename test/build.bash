8cf8b5232e9a749ba902b59f5bc9d524db369043
lisachandra <lisachandra's email is private>
2022-01-15 15:48:22 +0700

    fix array

--- test/build.bash
+++ test/build.bash
@@ -14,7 +14,7 @@ fi
 
 declare -A DEPENDENCIES
 
-while read line; do
+while IFS= read -r line; do
     KEY=$(awk -v LINE="$line" -F '[@/"]' '{if (NR == LINE); print $2 "_" $3}' DEPENDENCIES.toml)
     VALUE=$(awk -v LINE="$line" '{if (NR == LINE); print $1}' DEPENDENCIES.toml)
 
@@ -28,10 +28,10 @@ mkdir Packages/_Index/zxibs_wrapperservice
 cd Packages/_Index/zxibs_wrapperservice
 
 for key in "${!DEPENDENCIES[@]}"; do
-    DEPENDENCY_NAME=$(awk FS='_' '{print $2}' <<< "$key")
-    DEPENDENCY_PATH_NAME=$(awk -v PATTERN="$key" '$0~PATTERN' <<< "$DEPENDENCIES_PATH_NAME")
+    DEPENDENCY_NAME=$(echo "$key" | awk FS='_' '{print $2}')
+    DEPENDENCY_PATH_NAME=$(echo "$DEPENDENCIES_PATH_NAME" | awk -v PATTERN="$key" '$0~PATTERN')
 
-    echo "return require(script.Parent.Parent['${DEPENDENCY_PATH_NAME}']['${DEPENDENCY_NAME}'])"
+    echo "return require(script.Parent.Parent['${DEPENDENCY_PATH_NAME}']['${DEPENDENCY_NAME}']) (${DEPENDENCIES[${key}]})"
     echo "return require(script.Parent.Parent['${DEPENDENCY_PATH_NAME}']['${DEPENDENCY_NAME}'])" > "${DEPENDENCIES[${key}]}.lua"
 done
 
