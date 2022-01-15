6cda4ef995278961c22bd3851f8e28b2cc1833c9
lisachandra <lisachandra's email is private>
2022-01-15 09:47:20 +0700

    fix array

--- test/build.bash
+++ test/build.bash
@@ -12,9 +12,10 @@ if [ ! -d "Packages" ]; then
     mkdir Packages/_Index
 fi
 
-DEPENDENCIES_LIST=$(ls Packages/_Index)
+declare -A DEPENDENCIES
 
-DEPENDENCIES=($(awk -F '[ =@/"]' '{print "["$2"_"$3"]="$1}' DEPENDENCIES.toml))
+DEPENDENCIES+=($(awk -F '[ =@/"]' '{print "["$2"_"$3"]="$1}' DEPENDENCIES.toml))
+DEPENDENCIES_LIST=$(ls Packages/_Index)
 
 mkdir Packages/_Index/zxibs_wrapperservice
 
