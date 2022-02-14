af07d4daa1a8446e51cfa7717a9d326ef971b307
lisachandra <lisachandra's email is private>
2022-02-14 09:43:23 +0700

    test new run script

--- test/run.bash
+++ test/run.bash
@@ -5,14 +5,12 @@ EXIT_CODE=0
 while true; do
     run-in-roblox --place test/wrapperservice-test.rbxlx --script test/runner.server.lua > output.txt
 
-    if [ ! -z "$(grep 'test nodes reported failures.' 'output.txt')" ]; then 
+    if grep -q 'test nodes reported failures.' 'output.txt'; then 
         EXIT_CODE=1
         break
-    elif [ "$(echo $?)" == '0' ]; then
+    elif ! grep -q '[ERROR run_in_roblox] receiving on a closed channel' 'output.txt' then
         break
     fi
-
-    cat output.txt
 done
 
 cat output.txt
