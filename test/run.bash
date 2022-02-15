b0350b4d35f907e8f76e3868a39976ebb7e93379
lisachandra <lisachandra's email is private>
2022-02-15 09:42:40 +0700

    fix run script

--- test/run.bash
+++ test/run.bash
@@ -5,10 +5,10 @@ EXIT_CODE=0
 while true; do
     run-in-roblox --place test/wrapperservice-test.rbxlx --script test/runner.server.lua > output.txt
 
-    if grep -q '[-] Tests' 'output.txt'; then 
+    if grep -q '\[-\] Tests' 'output.txt'; then 
         EXIT_CODE=1
         break
-    elif grep -q '[+] Tests' 'output.txt'; then
+    elif grep -q '\[+\] Tests' 'output.txt'; then
         break
     fi
 done
